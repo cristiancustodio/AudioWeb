1966d511842416de594806146a068c5fc404cf10
Cristian Luciano Custodio <cristian.custodio@benner.com.br>
2025-07-08 10:11:49 -0300

    .

--- AudioWeb.Shared/Models/AudiometroModel.cs
+++ AudioWeb.Shared/Models/AudiometroModel.cs
@@ -1,8 +1,16 @@
-    namespace AudioWeb.Shared.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
-    public class AudiometroModel
+
+namespace AudioWeb.Shared.Models;
+
+public class AudiometroModel
 {
     public int Id { get; set; }
+
+    [Required]
+    [StringLength(100)]
+    [Column(TypeName = "varchar(100)")]
     public string Descricao { get; set; } = string.Empty;
     public DateTime? DataCalibracao { get; set; }
     public DateTime? DataAfericao { get; set; }
